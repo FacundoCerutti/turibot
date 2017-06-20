52d6009a120aebde70d3de6dbe24a443f8e3e681
Edgar Gerez <MartinchoooG19@hotmail.com>
2017-06-19 21:43:58 -0300

    genetico

--- TuriBoBot/Dialogs/RootDialog.cs
+++ TuriBoBot/Dialogs/RootDialog.cs
@@ -4,13 +4,20 @@ using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
 using TuriBoBot.Model;
+using System.Linq;
+using GAF;
+using GAF.Extensions;
+using GAF.Operators;
 
 namespace TuriBoBot.Dialogs
 {
     [Serializable]
     public class RootDialog : IDialog<object>
-    {            
-        private string name = "";
+    {
+
+
+
+private string name = "";
         object respuesta1 = new object();
         object respuesta2 = new object();
         object respuesta3 = new object();
@@ -549,5 +556,33 @@ namespace TuriBoBot.Dialogs
             }
             return Budgetcontrolar(values);
         }
+
+
+        /*private static IEnumerable<Destino> CreateCities()
+        {
+            var cities = new List<Destino>
+            {
+                new Destino("Birmingham", 52.486125, -1.890507),
+                new Destino("Bristol", 51.460852, -2.588139),
+                new Destino("London", 51.512161, -0.116215),
+                new Destino("Leeds", 53.803895, -1.549931),
+                new Destino("Manchester", 53.478239, -2.258549),
+                new Destino("Liverpool", 53.409532, -3.000126),
+                new Destino("Hull", 53.751959, -0.335941),
+                new Destino("Newcastle", 54.980766, -1.615849),
+                new Destino("Carlisle", 54.892406, -2.923222),
+                new Destino("Edinburgh", 55.958426, -3.186893),
+                new Destino("Glasgow", 55.862982, -4.263554),
+                new Destino("Cardiff", 51.488224, -3.186893),
+                new Destino("Swansea", 51.624837, -3.94495),
+                new Destino("Exeter", 50.726024, -3.543949),
+                new Destino("Falmouth", 50.152266, -5.065556),
+                new Destino("Canterbury", 51.289406, 1.075802)
+            };
+
+            return cities;
+        }/*
+
+
     }
 }
